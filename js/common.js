181db02328337ad9f59ce9e614f6f33dce297e21
jeong_euiran <90823391+uiran030@users.noreply.github.com>
2022-03-30 16:55:21 +0900

    20220330

--- js/common.js
+++ js/common.js
@@ -8,8 +8,17 @@ $('.bfiveInner').slick({
     autoplaySpeed:2000,
     speed:500,
     arrow:true,
-    prevArrow:'<button class="slick-arrow slick-prev"><i class="fas fa-angle-left"></i></button>',
-    nextArrow : '<button class="slick-arrow slick-next"><i class="fas fa-angle-right"></i></button>',
-    dots:true,
+    prevArrow:'<button class="slick-arrow slick-prev"><i class="fa-solid fa-circle-chevron-left"></i></button>',
+    nextArrow : '<button class="slick-arrow slick-next"><i class="fa-solid fa-circle-chevron-right"></i></button>',
+    dots:false,
     fade:true
+})
+
+// list6 ranking
+$('.ranking_btn > a').on('click', function(){
+    var aindex = $(this).index()
+    $(this).addClass('on').siblings().removeClass('on')
+    $('#ranking_list > table').eq(aindex).addClass('on').siblings().removeClass('on')
+
+    return false
 })
